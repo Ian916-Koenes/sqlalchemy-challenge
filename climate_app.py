4bb6b6857c8496422afdc8898bcb65e04b54e6e8
Ian_Koenes <iankoenes@gmail.com>
2020-12-14 21:04:07 -0800

    added api code

---
+++ climate_app.py
@@ -0,0 +1,37 @@
+from matplotlib import style
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# We can view all of the classes that automap found
+Base.classes.keys()
+
+# Save references to each table
+Measurement = Base.classes.measurement 
+Station = Base.classes.station 
+
+# Create our session (link) from Python to the DB
+session = Session(engine) 
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"/api/v1.0/precipitation"
+        f"/api/v1.0/stations"
+        f"/api/v1.0/tobs"
+        f"/api/v1.0/<start>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+
