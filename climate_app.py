6dbfa1d1296173729f97f2a72927935bccaf0e51
Ian_Koenes <iankoenes@gmail.com>
2020-12-15 18:24:44 -0800

    added dict api

--- climate_app.py
+++ climate_app.py
@@ -7,6 +7,10 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -23,15 +27,39 @@ Station = Base.classes.station
 # Create our session (link) from Python to the DB
 session = Session(engine) 
 
+app = Flask(__name__)
+
 @app.route("/")
 def welcome():
     """List all available api routes."""
     return (
-        f"/api/v1.0/precipitation"
-        f"/api/v1.0/stations"
-        f"/api/v1.0/tobs"
-        f"/api/v1.0/<start>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
         f"/api/v1.0/<start>/<end>"
     )
 
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all percipitarion values"""
+    # Query all precipitations
+    results = session.query(Measurement.date, Measurement.prcp).all()
+
+    session.close()
+
+    all_precipitations = {}
+    for date, prcp in results:
+        all_precipitations[date] = prcp
+       
+
+
+    return jsonify(all_precipitations)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
